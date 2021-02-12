e7bf6c166174db74b3ad0e50710de4fa855a9cbf
DavidWood2001 <davidwood1301@gmail.com>
2021-02-12 13:00:10 +0000

    Task blinky completed
    Blinky task completed

--- Tasks/HelloWorld/main.cpp
+++ Tasks/HelloWorld/main.cpp
@@ -1,19 +1,17 @@
-#include "mbed.h"
+#include "../lib/uopmsb/uop_msb_2_0_0.h"
 
-// main() runs in its own thread in the OS
-int main()
-{
-    printf("Hello World\n");
+using namespace uop_msb_200;
 
-    int a;
+LCD_16X2_DISPLAY display;
 
-    for (a=0; a < 5; a = a + 1) 
-    {
-        printf("a = %d\n",a);
-    }
-
-    printf("Final value of a is %d\n", a);
-
+void helloSimple(){
+    printf("Hello Console\n");
+    display.printf("Hello LCD");
     while (true);
 }
 
+// main() runs in its own thread in the OS
+int main(void)
+{
+    helloSimple();
+}
--- Tasks/Task-101-Blinky/main.cpp
+++ Tasks/Task-101-Blinky/main.cpp
@@ -6,27 +6,38 @@ DigitalOut greenLED(LED1);
 
 //These are "commented out" and so are not part of the program. You can uncomment them by removing the // characters
 //Your task is to make a sequence alternating between Green+Red and just Blue 
-//DigitalOut blueLED(LED2);
-//DigitalOut redLED(LED3);
+DigitalOut blueLED(LED2);
+DigitalOut redLED(LED3);
 
 //The main function - all executable C / C++ applications have a main function. This is our entry point in the software
 int main() 
 {
+    // Write a message to the terminal
+    puts("Welcome to your Nucleo board");
+
+    int waitTime = 250000;
+
     // ALL the repeating code is contained in a  “while loop”
     while(true) 
     { 
         //The code between the { curly braces } is the code that is repeated forever
 
-        // Turn onboard LED ON  
+        // Turn onboard LED ON  (Green and red)
         greenLED = 1; 
+        redLED = 1;
+        // Make sure LED is off (Blue)
+        blueLED = 0;
 
         // Wait 1 second (1 million microseconds)
-        wait_us(1000000); 
+        wait_us(waitTime); 
 
-        // Turn LED OFF
+        // Turn LED OFF (Green and Red)
         greenLED = 0;
+        redLED = 0;
+        // Turn LED on (Blue)
+        blueLED = 1;
 
         // Wait 1 second
-        wait_us(1000000); 
+        wait_us(waitTime); 
     }
 }
